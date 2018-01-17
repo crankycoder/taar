70710dc093431178a9c08d0e81f7f38aa08fe223
Victor Ng <victor@crankycoder.com>
2018-01-17 11:46:18 -0500

    More small porting updates to the PySpark script.
    This should complete most of the port from Scala to PySpark.
    
    We still need to push data into the DynamoDB but no more obvious
    syntax errors and the script will run to completion.

--- airflow/python/taar_dynamo.py
+++ airflow/python/taar_dynamo.py
@@ -11,53 +11,61 @@ https://github.com/mozilla/telemetry-batch-view/blob/1c544f65ad2852703883fe31a9f
 import sys
 import argparse
 from datetime import date
+from datetime import datetime
 from datetime import timedelta
 
-from pyspark.sql import functions as F
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import desc, row_number
 from pyspark.sql import Window
 
 
 def parse_args():
+    def valid_date_type(arg_date_str):
+        """custom argparse *date* type for user dates values given from the command line"""
+        try:
+            return datetime.strptime(arg_date_str, "%Y%m%d").date()
+        except ValueError:
+            msg = "Given Date ({0}) not valid! Expected format, YYYYMMDD!".format(arg_date_str)
+            raise argparse.ArgumentTypeError(msg)
+
     today_str = date.today().strftime("%Y%m%d")
     yesterday_str = (date.today()-timedelta(days=1)).strftime("%Y%m%d")
 
-    desc = 'Copy data from telemetry HBase to DynamoDB'
-    parser = argparse.ArgumentParser(description=desc)
+    description = 'Copy data from telemetry S3 parquet files to DynamoDB'
+    parser = argparse.ArgumentParser(description=description)
 
     parser.add_argument('--from',
             dest='from_date',
             action='store',
-            default=today_str,
+            type=valid_date_type,
+            default=yesterday_str,
             required=False,
-            help='Start date for data submission')
+            help='Start date for data submission (YYYYMMDD)')
 
     parser.add_argument('--to',
             dest='to_date',
+            type=valid_date_type,
             action='store',
-            default=today_str,
+            default=yesterday_str,
             required=False,
             help='End date of the data submission')
-
-    parser.add_argument('--hbase-master',
-            dest='hbase_master',
-            action='store',
-            required=True,
-            help='IP address of HBase master')
 
     args = parser.parse_args()
     return args
 
-def etl(from_date, to_date, dataFrame):
-    errAcc = spark.sparkContext.accumulator("Validation Errors")
+def etl(spark, from_date, to_date, dataFrameFunc):
+    # Validation Errors
+    errAcc = spark.sparkContext.accumulator(0)
 
-    for offset in range(0, to_date-from_date):
-        currentDate = from_date + offset
-        currentDateString = currentDate.strformat("%Y%m%d")
+    for offset in range(0, (to_date-from_date).days+1):
+        currentDate = from_date + timedelta(days=offset)
+        currentDateString = currentDate.strftime("%Y%m%d")
         print("Processing %s" % currentDateString)
 
 
         # Get the data for the desired date out of the dataframe
-        data = dataFrame(currentDateString)
+        datasetForDate = dataFrameFunc(currentDateString)
 
         # Get the most recent (client_id, subsession_start_date) tuple
         # for each client since the main_summary might contain
@@ -65,16 +73,15 @@ def etl(from_date, to_date, dataFrame):
         # full table with all the columns we require.
 
         # TODO: verify the '$' notation for pyspark vs scala spark
-        clientShortList = data.select("client_id",
-                "subsession_start_date",
-                F.row_number()
-                    .over(Window.partitionBy("client_id")
-                        .orderBy(F.desc("subsession_start_date")))
-                    .alias("clientid_rank"))
-                .where("clientid_rank" == 1)
-                .drop("clientid_rank")
+        clientShortList = datasetForDate.select("client_id", 
+                                                'subsession_start_date', 
+                                                row_number().over(
+                                                    Window.partitionBy('client_id').orderBy(desc('subsession_start_date'))
+                                                ).alias('clientid_rank')
+                          ).where('clientid_rank == 1').drop('clientid_rank')
 
-        dataSubset = data.select(
+
+        dataSubset = datasetForDate.select(
                 "client_id",
                 "subsession_start_date",
                 "subsession_length",
@@ -88,73 +95,78 @@ def etl(from_date, to_date, dataFrame):
                 "active_addons",
                 "disabled_addons_ids")
 
-         # Join the two tables: only the elements in both dataframes
-         # will make it through.
+        # Join the two tables: only the elements in both dataframes
+        # will make it through.
 
-         # TODO: Check that scala.collection.Seq is safely ported to
-         # vanilla Python lists
-         clientsData = dataSubset.join(
-                 clientsShortList, ["client_id", 'subsession_start_date'])
-
-
-          # Convert the DataFrame to JSON and get an RDD out of it.
-          subset = clientsData.select("client_id", "subsession_start_date")
-
-          # TODO: This looks wrong.  Try this in the interpreter
-          jsonData = clientsData.select("city",
-                                        "subsession_start_date",
-                                        "subsession_length",
-                                        "locale",
-                                        "os",
-                                        "places_bookmarks_count",
-                                        "scalar_parent_browser_engagement_tab_open_event_count",
-                                        "scalar_parent_browser_engagement_total_uri_count",
-                                        "scalar_parent_browser_engagement_unique_domains_count",
-                                        "active_addons",
-                                        "disabled_addons_ids").toJSON().rdd
-
-          # Build an RDD containing (key, document) tuples: one per client.
-          rdd = subset.rdd.zip(jsonData).flatMap{ case (row, json) =>
-                  clientId = row.getString(0)
-                  startDate = row.getString(1)
-
-                  try {
-                      assert(clientId != null && startDate != null)
-                      // Validate the subsession start date: parsing will throw if
-                      // the date format is not valid.
-                      val dateFormatter = org.joda.time.format.ISODateTimeFormat.dateTime()
-                      dateFormatter.parseDateTime(startDate)
-                      Some((clientId, Seq(json)))
-                      } catch {
-                          case _: Throwable =>
-                          errorAcc.add(1)
-                          None
-                          }
-                      }
-
-        // Bulk-send the RDD to HBase.
-        rdd.toHBaseBulk(tableName, columnFamily, List(column))
-    println(s"${errorAcc.value} validation errors encountered")
+        # TODO: Check that scala.collection.Seq is safely ported to
+        # vanilla Python lists
+        clientsData = dataSubset.join(
+                clientShortList, ["client_id", 'subsession_start_date'])
 
 
+        # Convert the DataFrame to JSON and get an RDD out of it.
+        subset = clientsData.select("client_id", "subsession_start_date")
+
+        jsonDataRDD = clientsData.select("city",
+                                         "subsession_start_date",
+                                         "subsession_length",
+                                         "locale",
+                                         "os",
+                                         "places_bookmarks_count",
+                                         "scalar_parent_browser_engagement_tab_open_event_count",
+                                         "scalar_parent_browser_engagement_total_uri_count",
+                                         "scalar_parent_browser_engagement_unique_domains_count",
+                                         "active_addons",
+                                         "disabled_addons_ids").toJSON()
+
+         # Build an RDD containing (key, document) tuples: one per client.
+        def flatMapFunc(row, json): 
+            clientId = row.getString(0)
+            startDate = row.getString(1)
+
+            try:
+                assert (clientId != None and startDate != None)
+
+                # Validate the subsession start date: parsing will throw if
+                # the date format is not valid.
+                datetime.datetime.strptime(startDate, "%Y%m%d")
+                return ((clientId, Seq(json)))
+            except:
+                errorAcc.add(1)
+
+        rdd = subset.rdd.zip(jsonDataRDD).flatMap(flatMapFunc)
+
+        # TODO: Bulk-send the RDD to DynamoDB
+        #rdd.toHBaseBulk(tableName, columnFamily, List(column))
+        print("Final computed RDD: ", rdd)
+        print("%d validation errors encountered" % errAcc.value)
 
 
-
-def main(*args):
+def main(spark):
     """
     This script takes 3 command line arguments:
-
-     --hbase-master $hbase
      --from $date
      --to $date
     """
 
     args = parse_args()
-    etl(args.from_date, 
+    etl(spark,
+        args.from_date, 
         args.to_date, 
-        spark.read.parquet("s3://telemetry-parquet/main_summary/%(MainSummaryView.schemaVersion)s/submission_date_s3="))
+        lambda dateString: spark.read.parquet("s3://telemetry-parquet/main_summary/v4/submission_date_s3=%s" % dateString))
 
 
+if __name__ == "__main__":
+    APP_NAME = "HBaseAddonRecommenderView"
+    # Configure OPTIONS
+    conf = SparkConf().setAppName(APP_NAME)
+    conf = conf.setMaster("local[*]")
+    #in cluster this will be like
+    #"spark://ec2-0-17-03-078.compute-#1.amazonaws.com:7077"
 
-if __name__ == '__main__':
-    main(sys.argv)
+    spark = SparkSession.builder.config(conf=conf).getOrCreate()
+    print ("My spark session: %s" % spark)
+    # Execute Main functionality
+    main(spark)
+
+
