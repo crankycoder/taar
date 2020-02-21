e1ae014d005c10e080129cc14896040398330434
Victor Ng <victor@crankycoder.com>
2020-02-21 14:52:42 -0500

    Custom serialization to work around pickle+RLock
    Serialization of objects needs to work around threading.Lock objects
    which cannot be pickled.  This becomes an issue when running the
    ensemble recommender training in a spark cluster.

--- taar/recommenders/lazys3.py
+++ taar/recommenders/lazys3.py
@@ -30,6 +30,21 @@ class LazyJSONLoader:
 
         self.logger.info("{} loader is initialized".format(self._key_str))
 
+    def __getstate__(self):
+        # We need to override the pickling feature to work around
+        # serialization of locks
+        state = self.__dict__.copy()
+        del state['_lock']
+        return state
+
+    def __setstate__(self, state):
+        # We need to override the pickling feature to work around
+        # serialization of locks
+        self.__dict__.update(state)
+
+        # Add the lock back since it doesn't exist in the pickle
+        self._lock = threading.RLock()
+
     def force_expiry(self):
         msg = "Existing model for {} reset to 0. Model was:".format(
             self._key_str, str(self._cached_copy)
